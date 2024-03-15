2cf9ffdfe0bcd7eabc179745912c3751d053edaf
viniciustmgs <viniciustmgs@gmail.com>
2024-03-15 01:14:30 -0300

    variable gamma and beta; benchmark functions

--- metaheuristics/fireflyAlgorithm.py
+++ metaheuristics/fireflyAlgorithm.py
@@ -26,6 +26,22 @@ def evaluateRange(variable_range, solution):
             evaluated_solution.append(variable)
     return evaluated_solution
 
+def rosenbrockFunction(solution):
+    total_sum = 0
+    iteration = 0
+    while iteration < 5:
+        total_sum += 100*(solution[iteration+1]-solution[iteration]**2)**2+(solution[iteration]-1)**2
+        iteration += 1
+    return total_sum
+
+def rastriginFunction(solution):
+    total_sum = 0
+    a_consant = 10
+    for variable in solution:
+        total_sum += variable**2 - a_consant*math.cos(2*math.pi*variable)
+    final = total_sum + a_consant*6
+    return final
+
 def fitness(solution):
     best_solution = [45, 63, 83, 14, 27, 71]
     difference_sum = 0
@@ -50,9 +66,25 @@ def movement(firefly, target_firefly, beta, alpha):
 def calc_distance(i_firefly, j_firefly):
     difference_sum = 0
     for index, variable in enumerate(i_firefly):
-        difference_sum += (variable - j_firefly[index]) ** 2
+        difference_sum += (variable - j_firefly[index])**2
     r = math.sqrt(difference_sum)
     return r
+
+def newAlpha(alpha):
+    if (alpha > 0.002):
+        return alpha - 0.002
+    else:
+        return alpha
+    
+def newBeta0(beta0):
+    if (beta0 > 1):
+        new_beta0 = beta0 - 5
+        if new_beta0 <= 0:
+            return 1
+        else:
+            return new_beta0
+    else:
+        return beta0
 
 def fireflyAlgorithm(population_size, max_iteration):
     
@@ -68,7 +100,7 @@ def fireflyAlgorithm(population_size, max_iteration):
     #Definindo o coeficiente de absorção de luz
     gamma = 0.1
     #Definindo o alpha (step size)
-    alpha = 0.2
+    alpha = 0.5
     #Definindo a atratividade inicial
     beta0 = 100
     #Definindo o range das variáveis
@@ -76,36 +108,39 @@ def fireflyAlgorithm(population_size, max_iteration):
 
     i_firefly = 0
     j_firefly = 1
-    iteration = 1
+    iteration = 0
     evaluations = 10
     with open('output.txt', 'w') as file:
         file.write(f'População inicial: {fireflies}\n')
         file.write(f'Intensidade inicial: {light_intensities}\n')
         while iteration < max_iteration:
-            file.write(f'Começando a iteração {iteration}\n')
+            print(iteration)
+            #file.write(f'Começando a iteração {iteration}\n')
             while i_firefly < population_size:
-                file.write(f' AVALIANDO O VAGALUME {i_firefly}\n')
+                #file.write(f' AVALIANDO O VAGALUME {i_firefly}\n')
                 while j_firefly < population_size:
                     if (i_firefly != j_firefly):
-                        file.write(f'  Comparando com o vagalume {j_firefly}\n')
+                        #file.write(f'  Comparando com o vagalume {j_firefly}\n')
                         #Caso o vagalume j seja mais luminoso que o vagalume i
                         if(light_intensities[i_firefly] < light_intensities[j_firefly]):
-                            file.write(f'    O vagalume {j_firefly} é mais luminoso que o vagalume {i_firefly}\n')
+                            #file.write(f'    O vagalume {j_firefly} é mais luminoso que o vagalume {i_firefly}\n')
+                            #file.write(f'    Vagalume i: {fireflies[i_firefly]}\n')
+                            #file.write(f'    Vagalume j: {fireflies[j_firefly]}')
                             #Calculando a distância entre os vagalumes
                             r = calc_distance(fireflies[i_firefly], fireflies[j_firefly])
-                            file.write(f'    A distância entre eles é de {r}\n')
+                            #file.write(f'    A distância entre eles é de {r}\n')
                             #Calculando a atratividade
                             beta = relativeAttractiveness(beta0, gamma, r)
-                            file.write(f'    A atratividade entre eles é {beta}\n')
+                            #file.write(f'    A atratividade entre eles é {beta}\n')
                             #Movendo o vagalume i em direção ao j
                             fireflies[i_firefly] = movement(fireflies[i_firefly], fireflies[j_firefly], beta, alpha)
                             #Avaliando o range das variáveis
                             fireflies[i_firefly] = evaluateRange(variable_range, fireflies[i_firefly])
-                            file.write(f'    O novo vagalume {i_firefly} é {fireflies[i_firefly]}\n')
+                            #file.write(f'    O novo vagalume {i_firefly} é {fireflies[i_firefly]}\n')
                             #Avaliando o novo vagalume
                             light_intensities[i_firefly] = fitness(fireflies[i_firefly])
                             evaluations += 1
-                            file.write(f'    Sua nova intensidade é {light_intensities[i_firefly]}\n')
+                            #file.write(f'    Sua nova intensidade é {light_intensities[i_firefly]}\n')
                     j_firefly += 1
                 i_firefly +=1
                 j_firefly = 0
@@ -115,6 +150,15 @@ def fireflyAlgorithm(population_size, max_iteration):
             file.write(f'Intensidades atuais: {light_intensities}\n')
             file.write(f'Numero de avaliações: {evaluations}\n')
             iteration += 1
+            alpha = newAlpha(alpha)
+            file.write(f'Novo alpha: {alpha}\n')
+            beta0 = newBeta0(beta0)
+        numero_mais_proximo_de_zero = min(light_intensities, key=lambda x: abs(x))
+        indice_numero = light_intensities.index(numero_mais_proximo_de_zero)
+        #file.write(f'{light_intensities}\n')
+        #file.write(f'{fireflies}\n')
+        file.write(f'Numero mais proximo de zero: {numero_mais_proximo_de_zero}\n')
+        file.write(f'Solução: {fireflies[indice_numero]}\n')
     
 
 
